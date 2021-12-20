7950351190c38af7454c5d2c0a5b9afd403a898c
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2021-12-21 04:27:58 +0500

    today my learning on react.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 import react from 'react';
 
 function App() {
+  const firstname = 'John';
+  const age = 28;
+  const job = "new job";
+  
+  const placeholder ="Enter your details"
+  const getFullName = (firstname) => `${firstname}`
+  const inputFieldBox = <input placeholder={placeholder} autoComplete/>
+ const mArry = [1,2,3,4,5];
+ const mObj = {
+   name: 'furqan',
+   age: 18,
+   job: 'programmer'
+ };
   return (
-   <h1>Hello World!</h1>
+   <div className="App">
+     <h1>first name : {getFullName(firstname)}</h1>
+   <p>age : {mObj.age}</p>
+   <h3> job : {mObj.job}</h3>
+
+   <input placeholder={placeholder} autoComplete/>
+ {inputFieldBox} <br></br>
+ {mArry[2]}  <br></br>
+ {mArry[1]>0?"true":"false"} 
+   </div>
   );
 }
 
