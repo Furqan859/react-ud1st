30c44fdd68af9ac338c1b63ee10254839cf32ceb
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2021-12-30 05:32:35 +0500

    practise useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  let name = "Furqan Rasool";
+  const [data , setData] = useState("Furqan")
   function todo () {
-    alert("Furqan")
+  setData("Rasool")
   }
   
   return (
      
     <div>
-<h1>{name}</h1>
+<h1>{data}</h1>
 <button onClick={()=>todo()} >CLick me</button>
-<br></br> <br></br>
 
-<button onClick={todo}>CLick To Show</button>
 
     </div>
   );
