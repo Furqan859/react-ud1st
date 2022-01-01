8b8934a1532dd27061a8b247c8f928f4cf93e9cf
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-02 02:17:59 +0500

    create toggle on one button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [data , setData] = useState(true)
+  const [data , setData] = useState()
  
   return (
     <div>
       {data?<h1>Hello World</h1>:null}
-<button onClick={()=>setData(true)}>Show</button>
-<button onClick={()=>setData(false)}>Hide</button>
+<button onClick={()=>setData(!data)}>Toggle</button>
+{/* <button onClick={()=>setData(false)}>Hide</button> */}
 
     </div>
   );
