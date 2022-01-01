4fcb6561b2f987227b0a9396f1a45a3aea459920
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-02 03:46:03 +0500

    invoke function from parent to diffrent child

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import child1 from './child1';
+import child2 from './child2';
 
 function App() {
   const one = () => <h1>hello child 1 to 1</h1> ; 
   return (
     <div>
-      <child1 one>hello child 1 to 1</child1>
+      <child1 one>parent function</child1><br/> <br/>
+      <child2 one>parent function</child2>
     </div>
   );
 }
---
+++ src/child2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function child2(props) {
+    return (
+        <div>
+            {props.one}
+        </div>
+    );
+}
+
+export default child2;
