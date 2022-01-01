81e2583e8d08cd83d836dde7a8853ee3bf1a8fee
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-02 01:44:28 +0500

    make update value in a single state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React,{useState} from 'react';
 
-
 function App() {
-  const [name , setName] = useState(null)
 
-  const [ print ,setPrint] = useState(false)
+  const [name, setName] = useState(false)
+
   
- 
+  const getData = (val) =>{ 
+    let info = (val.target.value)   
+    // setName(val.target.value)
+    // (false)
+    // console.log(val.target.value);
 
- 
+    return {...info};
+    // setPrint(false)
+   }
 
- const getdata = (val)=> {setName(val.target.value)
- setPrint(false) }
+  const clickHandle = () =>{
+    console.log(getData())
+  
+     setName(true)
+     
+
+
+      
+      // setName(a)
+  }
 
   return (
- <div>      
-          {
-            print ? <h1>{name}</h1> : null
-          }
-          <input onChange={getdata}></input>
-        <button onClick={()=>setPrint(true)}>Click Me</button>
-       </div>
+    <div>
+        <h1>{name}</h1>
+      <button onClick={clickHandle}>Click Me</button>
+      <input onChange={getData} />
+    </div>
   );
 }
 
