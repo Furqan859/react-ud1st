13cd40ec011b088b548ef3beba29515dfd4abb80
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-02 03:43:36 +0500

    passing function from parent to child

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React,{useState} from 'react';
+import React from 'react';
+import child1 from './child1';
 
 function App() {
-  const [name ,setName] = useState("");
-  const [tnc ,setTnc] = useState(false);
-  const [interest , setInterest] = useState("");
-  const onFormData = (e) => {
-    e.preventDefault()
-     console.log(name,tnc,interest);}
+  const one = () => <h1>hello child 1 to 1</h1> ; 
   return (
     <div>
-      <h1>Handle Form React</h1>
-     <form onSubmit={onFormData}>
-     <input type="text" placeholder="enter name" onChange={(e)=>setName(e.target.value)} />  <br />  <br />
-     <select onChange={(e)=>setInterest(e.target.value)}> <br />  <br />
-      <option>Selet Option</option>
-       <option>Marvel</option>
-       <option>DC</option>
-       <option>Network</option>
-     </select>
-     <br />  <br />
-     <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)} /><span>Accept Terms and Condions</span>
-     <br />  <br />
-
-     <button type="submit">Submit</button>
-     </form>
+      <child1 one>hello child 1 to 1</child1>
     </div>
   );
 }
---
+++ src/child1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function child1(props) {
+    return (
+        <div>
+            <h1>{props.one}</h1>
+        </div>
+    );
+}
+
+export default child1;
