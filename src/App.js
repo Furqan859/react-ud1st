727f32dd8186e4cf913fe345894cfb88bdd1e1f0
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-04 03:32:31 +0500

    MAke a table in bootstrap

--- src/App.js
+++ src/App.js
@@ -4,35 +4,64 @@ import {Table} from 'react-bootstrap';
 
 function App() {
   const user = [
-    {Name:"Furqan" , Email:"furqanrasool2@gmail.com" , Contact:"03055673859"},
-    {Name:"Usman" , Email:"usman@gmail.com" , Contact:"03127221493"},
-    {Name:"Ahmed" , Email:"ahmed@gmail.com" , Contact:"03164252198"},
-    {Name:"Ali" , Email:"ali@gmail.com" , Contact:"0309869867"}
+    {name:"Furqan" , email:"furqanrasool2@gmail.com" , contact:"03055673859" ,address:[{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"}]},
+    {name:"Usman" , email:"usman@gmail.com" , contact:"03127221493",address:[{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},]},
+    {name:"Ahmed" , email:"ahmed@gmail.com" , contact:"03164252198",address:[{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"}]},
+    {name:"Ali" , email:"ali@gmail.com" , contact:"0309869867",address:[{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"},{houseNumber:"10D",state:"Punjab",city:"Lahore",country:"Pakistan"}]}
   ]
-  
-  return (
-    <div>
-      <Table striped hover variant='dark' size='lg'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Conatct</th>
+   
+  return (<div>
+     <h1 style={{marginLeft:"40%"}}>Table Data </h1>
+     <Table  striped>
+       <thead>
+         <tr>
+           <th>S.N</th>
+           <th>Name</th>
+           <th>Email</th>
+           <th>Contact</th>
+           <th>Address</th>
+         </tr>
+       </thead>
+       
+         {
+           user.map((item , i)=>
+            <tr key={i} >
+              <td>{i+1}</td>   
+            <td>{item.name}</td>
+            <td>{item.email}</td>
+            <td>{item.contact}</td>
+            <td>
+              <Table  striped variant="dark">
+                <thead>
+                  <tr>
+                    <th>House Number</th>
+                    <th>State</th>
+                    <th>CIty</th>
+                    <th>Country</th>
+                  </tr>
+                </thead>
+                <tbody>
+              {
+              item.address.map((data)=>
+              <tr>
+                <td>{data.houseNumber}</td>
+                <td>{data.state}</td>
+                <td>{data.city}</td>
+                <td>{data.country}</td>
+              </tr>
+              )
+              }
+              </tbody>
+              </Table>
+              </td>
+
+              
           </tr>
-          </thead>
-          <tbody>
-      {user.map((item)=>
-      
-          <tr>
-            <td>{item.Name}</td>
-            <td>{item.Email}</td>
-            <td>{item.Contact}</td>
-          </tr>
-        
-      )}
-      </tbody>
+           )
+         }
+         
       </Table>
-    </div>
+     </div>
   );
 }
 
