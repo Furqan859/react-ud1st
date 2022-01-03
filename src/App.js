99f96b17ad6fa2c72f59c83ba946795a96b939f7
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-04 01:12:16 +0500

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
+
+import {Button} from 'react-bootstrap';
 
 function App() {
+  const [update , setUpdate] = useState("hjhj")
+  
   return (
     <div>
-      <h1>hello world</h1>
+      <h1>{update}</h1>
+      <Button onClick={()=>setUpdate("abc")}>Click Me</Button>
     </div>
   );
 }
 
-export default App;
+export default App;         
