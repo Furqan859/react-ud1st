a77c74c72fbc63e7e0f5d2ce5b1f793c2167e427
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-04 01:42:06 +0500

    make a responsive in bootstrap and show dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React,{useState} from 'react';
+import React from 'react';
 
-import {Button} from 'react-bootstrap';
+import {Table} from 'react-bootstrap';
 
 function App() {
-  const [update , setUpdate] = useState("hjhj")
+  const user = [
+    {Name:"Furqan" , Email:"furqanrasool2@gmail.com" , Contact:"03055673859"},
+    {Name:"Usman" , Email:"usman@gmail.com" , Contact:"03127221493"},
+    {Name:"Ahmed" , Email:"ahmed@gmail.com" , Contact:"03164252198"},
+    {Name:"Ali" , Email:"ali@gmail.com" , Contact:"0309869867"}
+  ]
   
   return (
     <div>
-      <h1>{update}</h1>
-      <Button onClick={()=>setUpdate("abc")}>Click Me</Button>
+      <Table striped hover variant='dark' size='lg'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Conatct</th>
+          </tr>
+          </thead>
+          <tbody>
+      {user.map((item)=>
+      
+          <tr>
+            <td>{item.Name}</td>
+            <td>{item.Email}</td>
+            <td>{item.Contact}</td>
+          </tr>
+        
+      )}
+      </tbody>
+      </Table>
     </div>
   );
 }
