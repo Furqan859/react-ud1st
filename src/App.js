df24d4692cce69fa4d307e486a6a0497b848ea89
Furqan Rasool <65628215+Furqan859@users.noreply.github.com>
2022-01-10 02:30:06 +0500

    useMemo to manuplate dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import Child from './Child';
+import React,{useState,useMemo} from 'react';
 
 function App() {
-  function alertFunction(data) {
-    
-    alert(data)
-    
-  }
+  const [update , setUpdate] = useState(0);
+  const [item , setItem] = useState(10);
+  
+  const memofunction = useMemo(function multicount(){
+    console.log("multicount")
+    return update*5
+  },[update])
   return (
     <div>
-      <Child name={alertFunction}/>
+      {memofunction}
+      <h1>{update}</h1>
+      <h1>{item}</h1>
+      <button onClick={()=>setUpdate(update+1)}>update</button>
+      <button onClick={()=>setItem(item*10)} >multi</button>
     </div>
   );
 }
